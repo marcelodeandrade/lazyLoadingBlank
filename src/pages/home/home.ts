ebd50401f5269e3323cbc51802f871795adf395a
Marcelo <andrade.asmarcelo@gmail.com>
2017-05-18 19:02:00 -0300

    init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
